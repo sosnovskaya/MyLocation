64b35cb63a45d00427e855159a60bfcd16a2148d
Margarita Sosnovskaya <ricunsos@gmail.com>
2020-03-28 16:02:42 +0300

    Detect location with GPS and Network

--- app/src/main/java/ru/margaritasosnovskaya/mylocation/MainActivity.java
+++ app/src/main/java/ru/margaritasosnovskaya/mylocation/MainActivity.java
@@ -1,14 +1,165 @@
 package ru.margaritasosnovskaya.mylocation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_ACCESS_LOCATION = 99;
+
+
+    TextView tvEnabledGPS;
+    TextView tvStatusGPS;
+    TextView tvLocationGPS;
+    TextView tvEnabledNet;
+    TextView tvStatusNet;
+    TextView tvLocationNet;
+
+    private LocationManager locationManager;
+    StringBuilder sbGPS = new StringBuilder();
+    StringBuilder sbNet = new StringBuilder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvEnabledGPS = findViewById(R.id.tvEnabledGPS);
+        tvStatusGPS =  findViewById(R.id.tvStatusGPS);
+        tvLocationGPS =  findViewById(R.id.tvLocationGPS);
+        tvEnabledNet =  findViewById(R.id.tvEnabledNet);
+        tvStatusNet =  findViewById(R.id.tvStatusNet);
+        tvLocationNet = findViewById(R.id.tvLocationNet);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSION_ACCESS_LOCATION);
+        }
     }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_ACCESS_LOCATION:
+                if (!(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    Toast.makeText(this, "Need your location!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSION_ACCESS_LOCATION);
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                1000 * 10, 10, locationListener);
+
+        locationManager.requestLocationUpdates(
+                LocationManager.NETWORK_PROVIDER,
+                1000 * 10, 10, locationListener);
+
+        checkEnabled();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(locationListener);
+    }
+
+
+    private LocationListener locationListener = new LocationListener() {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            showLocation(location);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            checkEnabled();
+        }
+
+
+        @SuppressLint("MissingPermission")
+        @Override
+        public void onProviderEnabled(String provider) {
+            checkEnabled();
+            showLocation(locationManager.getLastKnownLocation(provider));
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            if (provider.equals(LocationManager.GPS_PROVIDER)) {
+                tvStatusGPS.setText("Status: " + status);
+            } else if (provider.equals(LocationManager.NETWORK_PROVIDER)) {
+                tvStatusNet.setText("Status: " + status);
+            }
+        }
+    };
+
+    private void showLocation(Location location) {
+        if (location == null)
+            return;
+        if (location.getProvider().equals(LocationManager.GPS_PROVIDER)) {
+            tvLocationGPS.setText(formatLocation(location));
+        } else if (location.getProvider().equals(
+                LocationManager.NETWORK_PROVIDER)) {
+            tvLocationNet.setText(formatLocation(location));
+        }
+    }
+
+    private String formatLocation(Location location) {
+        if (location == null)
+            return "";
+        return String.format(
+                "Coordinates: lat = %1$.4f, lon = %2$.4f, time = %3$tF %3$tT",
+                location.getLatitude(), location.getLongitude(), new Date(
+                        location.getTime()));
+    }
+
+    private void checkEnabled() {
+        tvEnabledGPS.setText("Enabled: "
+                + locationManager
+                .isProviderEnabled(LocationManager.GPS_PROVIDER));
+        tvEnabledNet.setText("Enabled: "
+                + locationManager
+                .isProviderEnabled(LocationManager.NETWORK_PROVIDER));
+    }
+
+    public void onClickLocationSettings(View view) {
+        startActivity(new Intent(
+                android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+    }
+
 }
